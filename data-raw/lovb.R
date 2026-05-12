27a0a8205ce080670275a9385b5266ed475c5dfc
Jeffrey Stevens <51374986+JeffreyRStevens@users.noreply.github.com>
2026-05-12 09:43:32 -0500

    Add LOVB 2026 data

--- data-raw/lovb.R
+++ data-raw/lovb.R
@@ -19,14 +19,22 @@ usethis::use_data(lovb_teams, overwrite = TRUE)
 
 # Create LOVB player data
 # lovb_player_data <- group_stats(league = "LOVB", year = 2025, level = "player")
-lovb_player_data2025 <- read.csv("data-csv/lovb_player_data_2025.csv")
-lovb_player_data <- lovb_player_data2025
-write.csv(lovb_player_data, "data-csv/lovb_player_data.csv")
+lovb_player_data_2025 <- read.csv("data-csv/lovb_player_data_2025.csv")
+lovb_player_data_2026 <- read.csv("data-csv/lovb_player_data_2026.csv")
+lovb_player_data <- dplyr::bind_rows(
+  lovb_player_data_2025,
+  lovb_player_data_2026
+)
+write.csv(lovb_player_data, "data-csv/lovb_player_data.csv", row.names = FALSE)
 usethis::use_data(lovb_player_data, overwrite = TRUE)
 
 # Create LOVB team data
 # lovb_team_data <- group_stats(league = "LOVB", year = 2025, level = "team")
-lovb_team_data2025 <- read.csv("data-csv/lovb_team_data_2025.csv")
-lovb_team_data <- lovb_team_data2025
-write.csv(lovb_team_data, "data-csv/lovb_team_data.csv")
+lovb_team_data_2025 <- read.csv("data-csv/lovb_team_data_2025.csv")
+lovb_team_data_2026 <- read.csv("data-csv/lovb_team_data_2026.csv")
+lovb_team_data <- dplyr::bind_rows(
+  lovb_team_data_2025,
+  lovb_team_data_2026
+)
+write.csv(lovb_team_data, "data-csv/lovb_team_data.csv", row.names = FALSE)
 usethis::use_data(lovb_team_data, overwrite = TRUE)
